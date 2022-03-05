224df2d962ed194e1d650327ff9d880016609293
Richard Tillies <rtillies@gmail.com>
2022-03-05 15:30:59 -0500

    Refactor Attendee #initialize

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -3,7 +3,7 @@ require "pry"
 class Attendee
   attr_reader :name, :budget
 
-  def initialize(name: this_name, budget: this_budget)
+  def initialize(name:, budget:)
     @name = name
     @budget = budget[1..-1].to_i
   end

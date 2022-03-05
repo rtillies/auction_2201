da2012b754cc709940f35466b8684a1ad68f3967
Richard Tillies <rtillies@gmail.com>
2022-03-05 14:07:02 -0500

    Refactor attendee: budget.to_i

--- lib/auction.rb
+++ lib/auction.rb
@@ -12,10 +12,14 @@ class Auction
   end
 
   def item_names
-    @items.map { |item| item.name  }
+    @items.map { |item| item.name }
   end
 
   def unpopular_items
     @items.select {|item| item.bids.count == 0}
+  end
+
+  def potential_revenue
+    @items.sum {|item| item.current_high_bid.to_i}
   end
 end

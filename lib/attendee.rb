cbf5e93fa6a24717711fcdc1f662012ce9985577
Richard Tillies <rtillies@gmail.com>
2022-03-05 13:17:49 -0500

    Attendee class

---
+++ lib/attendee.rb
@@ -0,0 +1,10 @@
+require "pry"
+
+class Attendee
+  attr_reader :name, :budget
+
+  def initialize(name: this_name, budget: this_budget)
+    @name = name
+    @budget = budget[1..-1].to_f
+  end
+end
